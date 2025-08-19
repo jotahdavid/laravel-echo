c90f772b7d0c33b2630cf6a6f485d16f225e82a6
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2025-08-19 14:45:09 -0300

    feat(config): remove default by broadcast driver

--- packages/react/src/config/index.ts
+++ packages/react/src/config/index.ts
@@ -31,48 +31,7 @@ const getEchoInstance = <T extends BroadcastDriver>(): Echo<T> => {
 export const configureEcho = <T extends BroadcastDriver>(
     config: EchoOptions<T>,
 ): void => {
-    const defaults: ConfigDefaults<BroadcastDriver> = {
-        reverb: {
-            broadcaster: "reverb",
-            key: import.meta.env.VITE_REVERB_APP_KEY,
-            wsHost: import.meta.env.VITE_REVERB_HOST,
-            wsPort: import.meta.env.VITE_REVERB_PORT,
-            wssPort: import.meta.env.VITE_REVERB_PORT,
-            forceTLS:
-                (import.meta.env.VITE_REVERB_SCHEME ?? "https") === "https",
-            enabledTransports: ["ws", "wss"],
-        },
-        pusher: {
-            broadcaster: "pusher",
-            key: import.meta.env.VITE_PUSHER_APP_KEY,
-            cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-            forceTLS: true,
-            wsHost: import.meta.env.VITE_PUSHER_HOST,
-            wsPort: import.meta.env.VITE_PUSHER_PORT,
-            wssPort: import.meta.env.VITE_PUSHER_PORT,
-            enabledTransports: ["ws", "wss"],
-        },
-        "socket.io": {
-            broadcaster: "socket.io",
-            host: import.meta.env.VITE_SOCKET_IO_HOST,
-        },
-        null: {
-            broadcaster: "null",
-        },
-        ably: {
-            broadcaster: "pusher",
-            key: import.meta.env.VITE_ABLY_PUBLIC_KEY,
-            wsHost: "realtime-pusher.ably.io",
-            wsPort: 443,
-            disableStats: true,
-            encrypted: true,
-        },
-    };
-
-    echoConfig = {
-        ...defaults[config.broadcaster],
-        ...config,
-    } as EchoOptions<BroadcastDriver>;
+    echoConfig = config as EchoOptions<BroadcastDriver>;
 
     // Reset the instance if it was already created
     if (echoInstance) {
